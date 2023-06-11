d0feebbb7eec8072ef3861c558867262420636ba
arch <arch@local>
2023-06-11 11:16:19 +0200

    add trailer buttons

--- frontend/src/pages/movies/movies.js
+++ frontend/src/pages/movies/movies.js
@@ -1,24 +1,34 @@
 import React from "react";
 import DataGrid, {
   Pager,
+  Button,
   Column,
   Editing,
   FilterRow,
   Lookup,
 } from "devextreme-react/data-grid";
-import { createStore } from 'devextreme-aspnet-data-nojquery';
-import { states } from '../../data/states'
+import { createStore } from "devextreme-aspnet-data-nojquery";
+import { states } from "../../data/states";
 import Box from "@mui/material/Box";
 
 const store = createStore({
-    key: "id",
-    loadUrl: "/api/v1/movies",
-    insertUrl: "/api/v1/movies/insert",
-    updateUrl: "/api/v1/movies/update",
-    deleteUrl: "/api/v1/movies/delete"
+  key: "id",
+  loadUrl: "/api/v1/movies",
+  insertUrl: "/api/v1/movies/insert",
+  updateUrl: "/api/v1/movies/update",
+  deleteUrl: "/api/v1/movies/delete",
 });
 
 export default function Movies() {
+  function openTrailer(e) {
+    try {
+      var title = e.row.data.title;
+      var url =
+        "https://www.youtube.com/results?search_query=" +
+        encodeURIComponent(title + " trailer");
+      window.open(url, "_blank").focus();
+    } catch {}
+  }
   return (
     <Box sx={{ m: 2 }}>
       <DataGrid
@@ -38,8 +48,16 @@ export default function Movies() {
         <Column dataField="title" caption="Title" dataType="string" />
         <Column dataField="longTitle" caption="Long Title" dataType="string" />
         <Column dataField="date" caption="Date" dataType="date" />
-        <Column dataField="state" caption="State" dataType="string" filterValue="New">
+        <Column
+          dataField="state"
+          caption="State"
+          dataType="string"
+          filterValue="New"
+        >
           <Lookup dataSource={states} valueExpr="ID" displayExpr="Name" />
+        </Column>
+        <Column type="buttons" caption="Trailer">
+          <Button hint="trailer" icon="video" onClick={openTrailer} />
         </Column>
       </DataGrid>
     </Box>
--- frontend/src/pages/serien/serien.js
+++ frontend/src/pages/serien/serien.js
@@ -1,25 +1,34 @@
 import React from "react";
 import DataGrid, {
   Pager,
+  Button,
   Column,
   Editing,
   FilterRow,
   Lookup,
 } from "devextreme-react/data-grid";
-import { createStore } from 'devextreme-aspnet-data-nojquery';
-import { states } from '../../data/states'
+import { createStore } from "devextreme-aspnet-data-nojquery";
+import { states } from "../../data/states";
 import Box from "@mui/material/Box";
 
-
 const store = createStore({
-    key: "id",
-    loadUrl: "/api/v1/serien",
-    insertUrl: "/api/v1/serien/insert",
-    updateUrl: "/api/v1/serien/update",
-    deleteUrl: "/api/v1/serien/delete"
+  key: "id",
+  loadUrl: "/api/v1/serien",
+  insertUrl: "/api/v1/serien/insert",
+  updateUrl: "/api/v1/serien/update",
+  deleteUrl: "/api/v1/serien/delete",
 });
 
 export default function Serien() {
+  function openTrailer(e) {
+    try {
+      var title = e.row.data.title;
+      var url =
+        "https://www.youtube.com/results?search_query=" +
+        encodeURIComponent(title + " trailer");
+      window.open(url, "_blank").focus();
+    } catch {}
+  }
   return (
     <Box sx={{ m: 2 }}>
       <DataGrid
@@ -37,11 +46,19 @@ export default function Serien() {
           allowDeleting={false}
         />
 
-        <Column dataField="title" caption="Title" dataType="string"/>
-        <Column dataField="season" caption="Season" dataType="number"/>
-        <Column dataField="date" caption="Date" dataType="date"/>
-        <Column dataField="state" caption="State" dataType="string" filterValue="New">
+        <Column dataField="title" caption="Title" dataType="string" />
+        <Column dataField="season" caption="Season" dataType="number" />
+        <Column dataField="date" caption="Date" dataType="date" />
+        <Column
+          dataField="state"
+          caption="State"
+          dataType="string"
+          filterValue="New"
+        >
           <Lookup dataSource={states} valueExpr="ID" displayExpr="Name" />
+        </Column>
+        <Column type="buttons" caption="Trailer">
+          <Button hint="trailer" icon="video" onClick={openTrailer} />
         </Column>
       </DataGrid>
     </Box>
